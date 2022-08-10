d3d7d8394b83fbaa708ca209944279f72147538b
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-08-11 00:02:04 +0900

    포스터 사이즈 변경

--- TMDBProject/New/NewTableViewCell.swift
+++ TMDBProject/New/NewTableViewCell.swift
@@ -29,7 +29,7 @@ class NewTableViewCell: UITableViewCell {
     func collectionViewlayout() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: 100, height: 130)
+        layout.itemSize = CGSize(width: 100, height: 150)
         layout.minimumLineSpacing = 16
         layout.minimumInteritemSpacing = 16
         layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
